cd1f941d2d7f9705bf6526b14b042f1521193ed1
Lee Jarvis <ljjarvis@gmail.com>
2013-02-28 15:29:07 +0000

    move sendgrid fix middleware into Gridhook namespace

--- lib/gridhook/engine.rb
+++ lib/gridhook/engine.rb
@@ -4,6 +4,6 @@ module Gridhook
   class Engine < ::Rails::Engine
     # Insert our SendGrid JSON fix middleware before ParamsParser
     # attempts to encode the JSON.
-    config.app_middleware.insert_before ActionDispatch::ParamsParser, 'UnfortunateSendgridFix'
+    config.app_middleware.insert_before ActionDispatch::ParamsParser, 'Gridhook::UnfortunateSendgridFix'
   end
 end
--- lib/gridhook/unfortunate_sendgrid_fix.rb
+++ lib/gridhook/unfortunate_sendgrid_fix.rb
@@ -1,12 +1,14 @@
-class UnfortunateSendgridFix
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    if env['PATH_INFO'] == Gridhook.config.event_receive_path
-      env['CONTENT_TYPE'] = 'text/plain'
+module Gridhook
+  class UnfortunateSendgridFix
+    def initialize(app)
+      @app = app
     end
-    @app.call(env)
+
+    def call(env)
+      if env['PATH_INFO'] == Gridhook.config.event_receive_path
+        env['CONTENT_TYPE'] = 'text/plain'
+      end
+      @app.call(env)
+    end
   end
 end
