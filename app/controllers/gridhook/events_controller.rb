c4dabc6c0f9a35ff7edac7d8f0a1bc49bdaf1826
Omar Skalli <chetane@gmail.com>
2015-02-24 19:42:15 -0800

    Add support for basic authentication

--- app/controllers/gridhook/events_controller.rb
+++ app/controllers/gridhook/events_controller.rb
@@ -1,8 +1,26 @@
 module Gridhook
   class EventsController < ActionController::Base
+
+    before_filter :authenticate
+
     def create
       Gridhook::Event.process(params)
       head :ok
+    end
+
+    private
+
+    def authenticate
+      if Gridhook.config.username || Gridhook.config.password
+        valid_auth = authenticate_with_http_basic do |username, password|
+          Gridhook.config.username == username &&
+          Gridhook.config.password == password
+        end
+
+        unless valid_auth
+          raise (Gridhook.config.auth_error || 'Unauthorized Access')
+        end
+      end
     end
   end
 end
