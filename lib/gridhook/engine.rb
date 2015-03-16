887b1e4dfa283c60d4108fa4a6a410141e2d57f7
Mike de Bock <mike.debock@masterwebber.nl>
2015-03-16 22:21:07 +0100

    add gridhook decorators as engine dependency

--- lib/gridhook/engine.rb
+++ lib/gridhook/engine.rb
@@ -1,4 +1,9 @@
 module Gridhook
   class Engine < ::Rails::Engine
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/gridhook/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
