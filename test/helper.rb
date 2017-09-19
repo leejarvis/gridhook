e860e09b0230a31891dd3c87d171634c8b66d504
Stanislav K <lunaticman@gmail.com>
2017-09-20 00:17:20 +0700

    some tweaks

--- test/helper.rb
+++ test/helper.rb
@@ -2,9 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 # require File.expand_path '../dummy/config/environment', __FILE__
 
 require 'minitest/autorun'
-require_relative '../../lib/gridhook'
+require_relative '../lib/gridhook'
 
-class TestCase < MiniTest::Unit::TestCase
+class TestCase < Minitest::Test
   def self.test(name, &block)
     define_method("test_#{name.gsub(/\W/, '_')}", &block) if block
   end
