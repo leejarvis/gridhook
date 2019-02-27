83cfed4113e511450404451ecc5b3209af6cd485
Guillaume Wrobel <guillaume.wrobel@yespark.fr>
2019-02-27 17:21:50 +0100

    Parameters no longer inherits from with_indifferent_access in Rails 5
    https://blog.bigbinary.com/2016/07/25/parameters-no-longer-inherit-from-hash-with-indifferent-access-in-rails-5.html

--- lib/gridhook/event.rb
+++ lib/gridhook/event.rb
@@ -22,7 +22,7 @@ module Gridhook
     attr_reader :attributes
 
     def initialize(attributes)
-      @attributes = attributes.with_indifferent_access
+      @attributes = attributes.to_h.with_indifferent_access
     end
 
     # An alias for returning the type of this event, ie:
