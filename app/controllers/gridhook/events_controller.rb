ade4364e42451a5ff95dba158832fd69b0c2bcdd
Guillaume Wrobel <guillaume.wrobel@yespark.fr>
2019-02-27 17:10:06 +0100

    Skip verify authenticity token

--- app/controllers/gridhook/events_controller.rb
+++ app/controllers/gridhook/events_controller.rb
@@ -1,5 +1,7 @@
 module Gridhook
   class EventsController < ActionController::Base
+    skip_before_action :verify_authenticity_token, raise: false
+
     def create
       Gridhook::Event.process(params)
       head :ok
